06710bd10e01eae4ee50b2edc242faa461836c58
rex <anderson.aristotle@gmail.com>
2019-03-11 17:13:33 -0400

    tested user contact creatation

--- curl-scripts/contacts/create-contact.sh
+++ curl-scripts/contacts/create-contact.sh
@@ -2,7 +2,7 @@ curl "http://localhost:4741/contacts" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "contact": {
       "name": "'"${NAME}"'",
